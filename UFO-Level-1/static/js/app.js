82a899c633cb15d9b5aca28c15ca94d16c1f98ef
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2021-04-06 19:48:08 -0400

    ufos

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody")
+function buildTable(data) {
+	tbody.html("");
+	data.forEach((dataRow) =>) {
+		const row = tbody.append("tr");
+		Object.values(dataRow).forEach((values)=> {
+			let cell = row.append("td");
+			cell.text(values)
+		});
+	}
+}
