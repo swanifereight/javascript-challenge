8130a6cf1294c40fbda16205d209a092061e3fb3
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2021-04-08 18:17:40 -0400

    fixed

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -5,17 +5,19 @@ var tableData = data;
 var tbody = d3.select("tbody")
 function buildTable(data) {
 	tbody.html("");
-	data.forEach((dataRow) =>) {
+	data.forEach ((dataRow) => {
 		const row = tbody.append("tr");
 		Object.values(dataRow).forEach((values)=> {
 			let cell = row.append("td");
-			cell.text(values)
-		});
-	}
+			cell.text(values);
+
+		}
+		);
+	});
 }
 
 function handleCLick() {
-	const date = d3.select("#datetime").property("value")
+	const date = d3.select("#datetime").property("value");
 	let filteredData = tableData;
 	if (datetime) {
 		filteredData = filteredData.filter(row => row.datetime == date);
